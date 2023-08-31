0f202ec2920352987f02e523d046cb953792b15b
Vijayshree Thakur <143580947+Vijayshreethakur@users.noreply.github.com>
2023-08-31 12:07:33 +0530

    Add files via upload

---
+++ vijayshree/js/main.js
@@ -0,0 +1,19 @@
+
+// this is for scroll Bar
+// header scroll
+let nav = document.querySelector(".nabvar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 50){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+//nav hide
+let navBar = document.querySelectorAll(".nav-link")
+let navCollapse = document.querySelector("navbar-collapse.collapse")
+navBar.forEach(function(e){
+    e.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
